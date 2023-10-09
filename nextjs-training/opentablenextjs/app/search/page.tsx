445814245fcd8e6eccf445c4273776253b8f6584
Zykie Rallos <zykie1018@gmail.com>
2023-10-09 15:23:50 +0800

    Feature: add programmatic navigation with useRouter hook

--- nextjs-training/opentablenextjs/app/search/page.tsx
+++ nextjs-training/opentablenextjs/app/search/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Search() {
   return (
     <main className="bg-gray-100 min-h-screen w-screen">
-      <main className="max-w-screen-2xl m-auto bg-white">
+      <main className="max-w-screen-2xl m-auto text-black bg-white">
         {/* NAVBAR */}
         <nav className="bg-white p-2 flex justify-between">
           <Link href="/" className="font-bold text-gray-700 text-2xl">
