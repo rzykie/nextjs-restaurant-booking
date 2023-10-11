1a6bd1c80ab959dda2933da5c21ee8e3009f00b1
Zykie Rallos <zykie1018@gmail.com>
2023-10-11 13:10:06 +0800

    Feature: add componenent for search page. fix import in Header component

--- nextjs-training/opentablenextjs/app/search/page.tsx
+++ nextjs-training/opentablenextjs/app/search/page.tsx
@@ -1,36 +1,13 @@
-import Link from "next/link";
+import NavBar from "../component/NavBar";
+import Header from "./components/Header";
 
 export default function Search() {
   return (
     <main className="bg-gray-100 min-h-screen w-screen">
       <main className="max-w-screen-2xl m-auto text-black bg-white">
-        {/* NAVBAR */}
-        <nav className="bg-white p-2 flex justify-between">
-          <Link href="/" className="font-bold text-gray-700 text-2xl">
-            OpenTable{" "}
-          </Link>
-          <div>
-            <div className="flex">
-              <button className="bg-blue-400 text-white border p-1 px-4 rounded mr-3">
-                Sign in
-              </button>
-              <button className="border p-1 px-4 rounded">Sign up</button>
-            </div>
-          </div>
-        </nav>
+        <NavBar />
         {/* HEADER */}
-        <div className="bg-gradient-to-r to-[#5f6984] from-[#0f1f47] p-2">
-          <div className="text-left text-lg py-3 m-auto flex justify-center">
-            <input
-              className="rounded  mr-3 p-2 w-[450px]"
-              type="text"
-              placeholder="State, city or town"
-            />
-            <button className="rounded bg-red-600 px-9 py-2 text-white">
-              Let's go
-            </button>
-          </div>
-        </div>
+        <Header />
         <div className="flex py-4 m-auto w-2/3 justify-between items-start">
           {/* SEARCH SIDE BAR */}
           <div className="w-1/5">
