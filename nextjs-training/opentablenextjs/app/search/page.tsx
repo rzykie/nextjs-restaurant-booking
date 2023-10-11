f35f0d2b1c4cc61a99f079cdaafa0f6a68228c57
Zykie Rallos <zykie1018@gmail.com>
2023-10-11 14:52:51 +0800

    Feature: add restaurant card component

--- nextjs-training/opentablenextjs/app/search/page.tsx
+++ nextjs-training/opentablenextjs/app/search/page.tsx
@@ -1,5 +1,6 @@
 import NavBar from "../component/NavBar";
 import Header from "./components/Header";
+import RestaurantCard from "./components/RestaurantCard";
 import SearchSideBar from "./components/SearchSideBar";
 
 export default function Search() {
@@ -12,32 +13,7 @@ export default function Search() {
         <div className="flex py-4 m-auto w-2/3 justify-between items-start">
           <SearchSideBar />
           <div className="w-5/6">
-            {/* RESAURANT CARD */}
-            <div className="border-b flex pb-5">
-              <img
-                src="https://images.otstatic.com/prod1/49153814/2/medium.jpg"
-                alt=""
-                className="w-44 rounded"
-              />
-              <div className="pl-5">
-                <h2 className="text-3xl">Aiāna Restaurant Collective</h2>
-                <div className="flex items-start">
-                  <div className="flex mb-2">*****</div>
-                  <p className="ml-2 text-sm">Awesome</p>
-                </div>
-                <div className="mb-9">
-                  <div className="font-light flex text-reg">
-                    <p className="mr-4">$$$</p>
-                    <p className="mr-4">Mexican</p>
-                    <p className="mr-4">Ottawa</p>
-                  </div>
-                </div>
-                <div className="text-red-600">
-                  <a href="">View more information</a>
-                </div>
-              </div>
-            </div>
-            {/* RESAURANT CAR */}
+            <RestaurantCard />
           </div>
         </div>
       </main>
